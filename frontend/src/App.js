75f8637d450fcd09016e6d548bf5802fdbc739d8
Sarah Manon Pradel <sapra18@student.sdu.dk>
2021-04-12 11:43:57 +0200

    Setup footer. Needs Github Icon, but eh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
 import Navbar from '../src/components/Navigation/navbar';
+import Footer from '../src/components/Footer/footer';
+
+import Production from '../src/containers/Production/index.js'
 
 function App() {
   return (
-    <Navbar/>
+    <BrowserRouter style={{margin:0}}>
+      <Navbar/>
+      {/* <Switch>
+        <Route path="/production" component={Production}/>
+        <Redirect from="/" to="/production"/>
+      </Switch> */}
+      <Footer/>
+    </BrowserRouter>
   );
 }
 
