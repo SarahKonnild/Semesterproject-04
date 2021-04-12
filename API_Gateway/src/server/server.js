26867202d017d914b4e4667851b6475fdceb94a6
Kasper Svane <svane.kasper20@gmail.com>
2021-04-12 09:54:49 +0200

    Setup API Gateway Server
    API Gateway setup to be used for checking functionality on other services

---
+++ API_Gateway/src/server/server.js
@@ -0,0 +1,44 @@
+//Importing dependencies
+const express = require('express');
+const cors = require('cors');
+require('typescript-require');
+
+const middlewares = require('../middleware/notFoundRoute');
+
+//Connection to Express for API and Setting Port for 5000
+const app = express();
+const port = process.env.PORT || 5000;
+
+//Middleware
+app.use(cors());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+//API Connection
+// const authenticationRouter = require('../../../Services/Authentication/src/routes/auth');
+// const batchesRouter = require('../../../Services/Batches/src/routes/batchesRoute');
+// const brewsterRouter = require('../../../Services/Brewster/src/routes/brewsterRoute');
+// const optimizationRouter = require('../../../Services/Optimization/src/routes/optimizationRoute');
+
+// app.use('/auth', authenticationRouter);
+// app.use('/batches', batchesRouter);
+// app.use('/brewster', brewsterRouter);
+// app.use('/optimization', optimizationRouter);
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to Group 2 Semesterproject',
+  });
+});
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+//Setting server to listen to Port 5000
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
